2f8207fdea9d5e497179138c4a83b5c7286b1041
Abbasov Jeyhun <abbasov0700@gmail.com>
2020-09-29 20:59:02 +0300

    lesson2

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -160,9 +160,9 @@ fun triangleKind(a: Double, b: Double, c: Double) =
  * Найти длину пересечения отрезков AB и CD.
  * Если пересечения нет, вернуть -1.
  */
-fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int {
-    return if (b in c..d && a <= c) b - c else
-        if (b <= d && a > c && d > a) b - a else
-            if (d <= b && a > c && d >= a) d - a else
-                if (d < b && a <= c && d >= a) d - c else -1
-}
+fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int =
+    if (b in c..d && a <= c) b - c
+    else if (b <= d && a > c && d > a) b - a
+    else if (d <= b && a > c && d >= a) d - a
+    else if (d < b && a <= c && d >= a) d - c
+    else -1
